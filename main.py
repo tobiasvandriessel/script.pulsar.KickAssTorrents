89a191b10f56864062eaf98bfe67113b0465eccb
tobiasvandriessel <tobiasvandriessel@openmailbox.org>
2014-09-15 17:53:16 +0200

    0.0.3

--- main.py
+++ main.py
@@ -8,22 +8,28 @@ import urllib2
 PAYLOAD = json.loads(base64.b64decode(sys.argv[1]))
 
 def search(query):
-    response = urllib2.urlopen("https://kickass.to/search?q=%s" % urllib.quote_plus(query))
+    response = urllib2.urlopen("http://kickass.to/search?q=%s" % urllib.quote_plus(query))
     data = response.read()
     if response.headers.get("Content-Encoding", "") == "gzip":
         import zlib
         data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
     return [{"uri": magnet} for magnet in re.findall(r'magnet:\?[^\'"\s<>\[\]]+', data)]
 
-
 def search_episode(imdb_id, tvdb_id, name, season, episode):
     return search("%s S%02dE%02d" % (name, season, episode))
 
 
 def search_movie(imdb_id, name, year):
-    return search(imdb_id)
+    response = urllib2.urlopen("http://kickass.to/usearch/" + name + "%20category:movies/")
+    data = response.read()
+    if response.headers.get("Content-Encoding", "") == "gzip":
+        import zlib
+        data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
+    return [{"uri": magnet} for magnet in re.findall(r'magnet:\?[^\'"\s<>\[\]]+', data)]
 
 urllib2.urlopen(
     PAYLOAD["callback_url"],
     data=json.dumps(globals()[PAYLOAD["method"]](*PAYLOAD["args"]))
 )
+
+
