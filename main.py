9f500805b7d1925ed81d3c1f98f6ca9e0e92f646
root <root@tobiaslinux.sshunet.nl>
2014-10-21 14:59:17 +0200

    indented errors fixed

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from pulsar import provider
 
 def search(query):
-resp = provider.GET("http://kickass.to/search?q=%s" % provider.quote_plus(query), params={
-"q": query,
-})
-return provider.extract_magnets(resp.data)
+	resp = provider.GET("http://kickass.to/search?q=%s" % provider.quote_plus(query), params={"q": query,})
+	return provider.extract_magnets(resp.data)
 
 def search_episode(episode):
-return search("%(title)s S%(season)02dE%(episode)02d" % episode)
+	return search("%(title)s S%(season)02dE%(episode)02d" % episode)
 
 def search_movie(movie):
-return search("%(title)s %(year)d" % movie)
+	return search("%(title)s %(year)d" % movie)
 
 provider.register(search, search_movie, search_episode)
 
