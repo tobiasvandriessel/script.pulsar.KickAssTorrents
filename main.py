13d0636d74d39520263af3706a0f08e8724d74da
tobiasvandriessel <tobiasvandriessel@users.noreply.github.com>
2015-01-02 16:21:49 +0100

    Update main.py
    Added a few suggested changes by ttcremers. Now it narrows the search results down to tv-shows, just to be sure, and it shows only results in 1080p or 720p.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pulsar import provider
 
 def search(query):
-	resp = provider.GET("http://kickass.to/search", params={
+	resp = provider.GET("http://kickass.to/usearch", params={
 		"q": query,
 	})
 	return provider.extract_magnets(resp.data)
 
 def search_episode(episode):
-	return search("%(title)s S%(season)02dE%(episode)02d" % episode)
+	return search("%(title)s S%(season)02dE%(episode)02d  category:tv 720p OR 1080p" % episode)
 
 def search_movie(movie):
 	return search("%(title)s %(year)d" % movie)
